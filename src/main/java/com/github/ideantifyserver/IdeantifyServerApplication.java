38758883e9c7bb8fa8fb8abd9403d9051f2aac8d
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-08-18 16:04:51 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/github/ideantifyserver/IdeantifyServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.ideantifyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeantifyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeantifyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ideantifyserver/IdeantifyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ideantifyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeantifyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
