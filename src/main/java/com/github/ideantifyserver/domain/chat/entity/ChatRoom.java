8a18207032adc498c6c4438cb5fcfedf7a1ca7e3
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-09-04 17:49:46 +0900

    [ID-18] feat(chat): chat 관련 엔티티 생성

---
+++ src/main/java/com/github/ideantifyserver/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,44 @@
+package com.github.ideantifyserver.domain.chat.entity;
+
+import com.github.ideantifyserver.domain.ideareport.entity.IdeaReportResult;
+import com.github.ideantifyserver.global.infra.mysql.BaseSchema;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder(toBuilder = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom extends BaseSchema {
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    ChatRoomType type;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @Builder.Default
+    List<ChatBot> chatBots = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idea_report_result_id")
+    IdeaReportResult ideaReport;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inner_project_id")
+    InnerProject project;
+
+    public enum ChatRoomType {
+        USER,
+        IDEA_REPORT,
+        PROJECT
+    }
+}
