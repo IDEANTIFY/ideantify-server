d341d807861daef9d65daca266a3dba854fa4a92
arirang030 <arirang030@kookmin.ac.kr>
2025-09-05 14:30:45 +0900

    [ID-02] refactor(jwt): key 사이즈 설정

--- src/main/java/com/github/ideantifyserver/global/property/JwtProperty.java
+++ src/main/java/com/github/ideantifyserver/global/property/JwtProperty.java
@@ -1,15 +1,19 @@
 package com.github.ideantifyserver.global.property;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
-@Data
+@Getter
+@Validated
 @Configuration
 @ConfigurationProperties(prefix = "app.jwt")
 public class JwtProperty {
 
+    @Size(min = 32)
     @NotBlank
     String key;
 }
