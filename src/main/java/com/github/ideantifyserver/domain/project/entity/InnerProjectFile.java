37113f5eff5f57f1778da7d7f29e50ea2661eb9b
Narae Yang <101495452+didskfo@users.noreply.github.com>
2025-09-07 01:06:37 +0900

    [0.1.0] ID-17 InnerProject 엔티티 생성 (#3)
    * [ID-17] feat(project): InnerProject 관련 도메인 생성
    
    * [ID-17] feat(project): lombok 추가
    
    * [ID-17] feat(project): EqualsAndHashCode lombok 제거
    
    * [ID-17] feat(project): EqualsAndHashCode lombok 추가
    
    * [ID-17] chore(gitignore): application.yaml 추가
    
    * [ID-17] feat(project): @EqualsAndHashCode 수정
    
    * [ID-17] chore(gitignore): .env 추가

---
+++ src/main/java/com/github/ideantifyserver/domain/project/entity/InnerProjectFile.java
@@ -0,0 +1,22 @@
+package com.github.ideantifyserver.domain.project.entity;
+
+import com.github.ideantifyserver.global.infra.mysql.BaseSchema;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder(toBuilder = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class InnerProjectFile extends BaseSchema {
+    @Column(nullable = false)
+    @NotBlank
+    String file;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    InnerProject project;
+}
