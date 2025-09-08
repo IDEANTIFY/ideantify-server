6eb7a8dc4cbc6686e9478e0cd72f24938432c5b8
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-09-09 01:43:29 +0900

    [ID-18] refactor(ideaReport): 검증 어노테이션 추가

--- src/main/java/com/github/ideantifyserver/domain/ideareport/entity/IdeaReportResultItem.java
+++ src/main/java/com/github/ideantifyserver/domain/ideareport/entity/IdeaReportResultItem.java
@@ -2,13 +2,14 @@ package com.github.ideantifyserver.domain.ideareport.entity;
 
 import com.github.ideantifyserver.global.infra.mysql.BaseSchema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
 @Getter
 @Builder(toBuilder = true)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class IdeaReportResultItem  extends BaseSchema {
@@ -26,15 +27,15 @@ public class IdeaReportResultItem  extends BaseSchema {
     String summary;
 
     @Column(nullable = false)
-    @NotBlank
+    @Min(0) @Max(100)
     int similarity;
 
     @Column(nullable = false)
-    @NotBlank
-    int creativiity;
+    @Min(0) @Max(100)
+    int creativity;
 
     @Column(nullable = false)
-    @NotBlank
+    @Min(0) @Max(100)
     int feasibility;
 
     @Column
