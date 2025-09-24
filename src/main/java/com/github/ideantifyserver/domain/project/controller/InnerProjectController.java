891690de34ce976002f5d0bd29391facd1b3a4fb
didskfo <didskfo2003@gmail.com>
2025-09-24 19:57:21 +0900

    [ID-21] chore(InnerProject): RequestMapping url 수정

--- src/main/java/com/github/ideantifyserver/domain/project/controller/InnerProjectController.java
+++ src/main/java/com/github/ideantifyserver/domain/project/controller/InnerProjectController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/projects")
+@RequestMapping("/projects")
 @RequiredArgsConstructor
 @Tag(name = "[Project]")
 public class InnerProjectController {
