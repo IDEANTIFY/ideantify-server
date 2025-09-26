370cfc0911b99d93269e32e7f334da18bd9a9904
didskfo <didskfo2003@gmail.com>
2025-09-26 23:04:45 +0900

    [ID-21] feat(project): 기본 생성자 추가

--- src/main/java/com/github/ideantifyserver/domain/project/dto/request/CreateProjectRequestDto.java
+++ src/main/java/com/github/ideantifyserver/domain/project/dto/request/CreateProjectRequestDto.java
@@ -1,14 +1,17 @@
 package com.github.ideantifyserver.domain.project.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateProjectRequestDto {
         @NotBlank
         String image;
