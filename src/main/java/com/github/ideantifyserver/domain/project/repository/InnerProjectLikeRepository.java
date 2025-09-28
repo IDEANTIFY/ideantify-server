414062637d9d0cbf2e95073857fbb93bcc77fdfe
didskfo <didskfo2003@gmail.com>
2025-09-28 16:41:07 +0900

    [ID-38] chore(project): import 정리

--- src/main/java/com/github/ideantifyserver/domain/project/repository/InnerProjectLikeRepository.java
+++ src/main/java/com/github/ideantifyserver/domain/project/repository/InnerProjectLikeRepository.java
@@ -3,7 +3,6 @@ package com.github.ideantifyserver.domain.project.repository;
 import com.github.ideantifyserver.domain.project.entity.InnerProjectLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface InnerProjectLikeRepository extends JpaRepository<InnerProjectLike, UUID> {
