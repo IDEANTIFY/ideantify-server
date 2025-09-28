29e5f4955308380fa0add64a83e94d86229a1796
didskfo <didskfo2003@gmail.com>
2025-09-28 19:23:59 +0900

    [ID-38] feat(project): 유니크 제약 추가

--- src/main/java/com/github/ideantifyserver/domain/project/entity/InnerProjectLike.java
+++ src/main/java/com/github/ideantifyserver/domain/project/entity/InnerProjectLike.java
@@ -2,13 +2,15 @@ package com.github.ideantifyserver.domain.project.entity;
 
 import com.github.ideantifyserver.domain.user.entity.User;
 import com.github.ideantifyserver.global.infra.mysql.BaseSchema;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_project_like", columnNames = {"project_id", "user_id"})
+        }
+)
 @Getter
 @Builder(toBuilder = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
