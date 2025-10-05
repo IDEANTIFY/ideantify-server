5b0f56f67d8bc778f363aaddc05bc278064b4916
arirang030 <arirang030@kookmin.ac.kr>
2025-10-06 03:08:30 +0900

    [ID-35] refactor(innerProject): @PreAuthorize("isAuthenticated()")를 서비스에서 컨트롤러로 이동

--- src/main/java/com/github/ideantifyserver/domain/project/controller/InnerProjectController.java
+++ src/main/java/com/github/ideantifyserver/domain/project/controller/InnerProjectController.java
@@ -17,10 +17,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
+@PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping("/projects")
 @RequiredArgsConstructor
--- src/main/java/com/github/ideantifyserver/domain/project/service/InnerProjectService.java
+++ src/main/java/com/github/ideantifyserver/domain/project/service/InnerProjectService.java
@@ -18,7 +18,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +25,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
-@PreAuthorize("isAuthenticated()")
 @RequiredArgsConstructor
 public class InnerProjectService {
     private final InnerProjectRepository innerProjectRepository;
