e7c9b50489e005d5012c1f0c71a906d2382909ed
arirang030 <arirang030@kookmin.ac.kr>
2025-10-06 23:40:09 +0900

    [ID-35] refactor(user): providers와 keywords 정보 따로 불러오기

--- src/main/java/com/github/ideantifyserver/domain/user/repository/UserRepository.java
+++ src/main/java/com/github/ideantifyserver/domain/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.github.ideantifyserver.domain.user.repository;
 
 import com.github.ideantifyserver.domain.user.entity.User;
 import org.springframework.data.domain.Limit;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +13,8 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    Optional<User> findByEmail(String email);
+    @EntityGraph(attributePaths = {"providers"})
+    Optional<User> findByEmail(@Param("email") String email);
 
     @Query("SELECT distinct u FROM User u WHERE :query IS NULL or u.nickname LIKE CONCAT('%', :query, '%') or u.email LIKE CONCAT('%', :query, '%')")
     List<User> findByNicknameOrEmail(@Param("query") String query, Limit limit);
--- src/main/java/com/github/ideantifyserver/global/security/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/github/ideantifyserver/global/security/oauth/CustomOAuth2UserService.java
@@ -1,13 +1,14 @@
 package com.github.ideantifyserver.global.security.oauth;
 
 import com.github.ideantifyserver.domain.user.entity.User;
+import com.github.ideantifyserver.domain.user.entity.UserDomain;
 import com.github.ideantifyserver.domain.user.entity.UserProvider;
 import com.github.ideantifyserver.domain.user.entity.UserSocial;
+import com.github.ideantifyserver.domain.user.repository.UserDomainRepository;
 import com.github.ideantifyserver.domain.user.repository.UserProviderRepository;
 import com.github.ideantifyserver.domain.user.repository.UserRepository;
 import com.github.ideantifyserver.domain.user.repository.UserSocialRepository;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Hibernate;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -16,6 +17,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
@@ -23,6 +26,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     private final UserRepository userRepository;
     private final UserProviderRepository userProviderRepository;
     private final UserSocialRepository userSocialRepository;
+    private final UserDomainRepository userDomainRepository;
 
     @Override
     @Transactional
@@ -41,11 +45,9 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         User user = userRepository.findByEmail(oAuth2UserInfo.getEmail())
                 .orElseGet(() -> createUserWithProvider(oAuth2UserInfo));
 
-        // Hibernate 강제 초기화
-        Hibernate.initialize(user.getProviders());
-        Hibernate.initialize(user.getKeywords());
+        List<UserDomain> domain = userDomainRepository.findByUser(user);
 
-        return new CustomOAuth2UserDetails(user, oAuth2User.getAttributes());
+        return new CustomOAuth2UserDetails(user, oAuth2User.getAttributes(), domain);
     }
 
     private User createUserWithProvider(OAuth2UserInfo oAuth2UserInfo) {
