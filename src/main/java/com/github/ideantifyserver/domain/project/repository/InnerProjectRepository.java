b0051f7139fe53f37fd51d44bd0325aabe9a76e4
arirang030 <arirang030@kookmin.ac.kr>
2025-10-06 23:43:44 +0900

    [ID-35] refactor(repository): @Param 삭제(필수 아님)

--- src/main/java/com/github/ideantifyserver/domain/project/repository/InnerProjectRepository.java
+++ src/main/java/com/github/ideantifyserver/domain/project/repository/InnerProjectRepository.java
@@ -1,21 +1,10 @@
 package com.github.ideantifyserver.domain.project.repository;
 
 import com.github.ideantifyserver.domain.project.entity.InnerProject;
-import com.github.ideantifyserver.domain.user.entity.User;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface InnerProjectRepository extends JpaRepository<InnerProject, UUID>, JpaSpecificationExecutor<InnerProject> {
-    @Query("""
-           select distinct p
-           from InnerProject p
-           left join p.members m
-           where (m.user = :user)
-           """)
-    List<InnerProject> findAllByMember(@Param("user") User user);
 }
--- src/main/java/com/github/ideantifyserver/domain/user/repository/UserRepository.java
+++ src/main/java/com/github/ideantifyserver/domain/user/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,8 +13,8 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     @EntityGraph(attributePaths = {"providers"})
-    Optional<User> findByEmail(@Param("email") String email);
+    Optional<User> findByEmail(String email);
 
     @Query("SELECT distinct u FROM User u WHERE :query IS NULL or u.nickname LIKE CONCAT('%', :query, '%') or u.email LIKE CONCAT('%', :query, '%')")
-    List<User> findByNicknameOrEmail(@Param("query") String query, Limit limit);
+    List<User> findByNicknameOrEmail(String query, Limit limit);
 }
