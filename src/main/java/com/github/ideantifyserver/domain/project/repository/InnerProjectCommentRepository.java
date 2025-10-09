02ab7131cf78eef5ad924b974c90d51c46b8ddae
didskfo <didskfo2003@gmail.com>
2025-10-09 23:16:33 +0900

    [ID-56] fix(project): param import 수정 및 lock mode type 변경

--- src/main/java/com/github/ideantifyserver/domain/project/repository/InnerProjectCommentRepository.java
+++ src/main/java/com/github/ideantifyserver/domain/project/repository/InnerProjectCommentRepository.java
@@ -1,19 +1,19 @@
 package com.github.ideantifyserver.domain.project.repository;
 
 import com.github.ideantifyserver.domain.project.entity.InnerProjectComment;
-import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface InnerProjectCommentRepository extends JpaRepository<InnerProjectComment, UUID> {
-    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("""
         select c
         from InnerProjectComment c
