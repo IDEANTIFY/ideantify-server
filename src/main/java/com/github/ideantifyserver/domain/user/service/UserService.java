fe6b9dcc31531bc07a930d496ff818a66ee2e513
arirang030 <145207957+arirang030@users.noreply.github.com>
2025-11-06 15:56:34 +0900

    [0.2.0] ID-47 유저 팔로우 및 팔로워/팔로잉 목록 조회 (#22)
    * [ID-47] feat(follow): 팔로우 api 구현
    
    * [ID-47] feat(follow): 언팔로우 api 구현 및 자기자신 팔로우 방지
    
    * [ID-47] feat(follow): 팔로워 & 팔로잉 목록 조회 api 구현
    
    * [ID-47] refactor(user): 로직 오류 수정
    
    * [ID-47] refactor(user): 닉네임 중복 방지

--- src/main/java/com/github/ideantifyserver/domain/user/service/UserService.java
+++ src/main/java/com/github/ideantifyserver/domain/user/service/UserService.java
@@ -4,9 +4,13 @@ import com.github.ideantifyserver.domain.auth.exception.AuthExceptions;
 import com.github.ideantifyserver.domain.user.dto.response.SimpleUserResponse;
 import com.github.ideantifyserver.domain.user.dto.response.UserResponse;
 import com.github.ideantifyserver.domain.user.entity.User;
+import com.github.ideantifyserver.domain.user.entity.UserFollow;
+import com.github.ideantifyserver.domain.user.repository.UserFollowRepository;
 import com.github.ideantifyserver.domain.user.repository.UserRepository;
+import com.github.ideantifyserver.global.exception.GlobalExceptions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Limit;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +21,7 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserFollowRepository userFollowRepository;
 
     @Transactional(readOnly = true)
     public UserResponse getMyProfile(User user) {
@@ -29,5 +34,45 @@ public class UserService {
 
         List<User> users = userRepository.findByNicknameOrEmail(query, Limit.of(5));
         return users.stream().map(SimpleUserResponse::from).toList();
+    }
+
+    @Transactional
+    @PreAuthorize("#me != #user")
+    public void followUser(User me, User user) {
+
+        if (userFollowRepository.existsByFollowerAndFollowing(me, user)) {
+            throw GlobalExceptions.NOT_PERMITTED.toException();
+        }
+
+        userFollowRepository.save(UserFollow.builder().follower(me).following(user).build());
+    }
+
+    @Transactional
+    public void unfollowUser(User me, User user) {
+
+        UserFollow userFollow = userFollowRepository.findByFollowerAndFollowing(me, user)
+                .orElseThrow(GlobalExceptions.NOT_PERMITTED::toException);
+
+        userFollowRepository.delete(userFollow);
+    }
+
+    public List<SimpleUserResponse> getFollowers(User me) {
+
+        List<UserFollow> follows = userFollowRepository.findByFollowing(me);
+
+        return follows.stream()
+                .map(UserFollow::getFollower)
+                .map(SimpleUserResponse::from)
+                .toList();
+    }
+
+    public List<SimpleUserResponse> getFollowings(User me) {
+
+        List<UserFollow> followings = userFollowRepository.findByFollower(me);
+
+        return followings.stream()
+                .map(UserFollow::getFollowing)
+                .map(SimpleUserResponse::from)
+                .toList();
     }
 }
