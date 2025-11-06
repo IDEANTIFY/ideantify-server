ea3c016d9d2ec5723e62002c673c32879437d8b6
didskfo <didskfo2003@gmail.com>
2025-11-06 18:50:56 +0900

    [ID-29] fix(ideaReport): sqs message type 수정

--- src/main/java/com/github/ideantifyserver/domain/ideareport/sqs/IdeaReportMetadataSqsGateway.java
+++ src/main/java/com/github/ideantifyserver/domain/ideareport/sqs/IdeaReportMetadataSqsGateway.java
@@ -4,6 +4,7 @@ import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class IdeaReportMetadataSqsGateway {
     private final SqsTemplate sqsTemplate;
 
@@ -18,7 +20,7 @@ public class IdeaReportMetadataSqsGateway {
     private String requestQueue;
 
     private static final String HDR_MESSAGE_TYPE = "messageType";
-    private static final String MESSAGE_TYPE     = "IDEA_REPORT";
+    private static final String MESSAGE_TYPE     = "IDEA_REPORT_METADATA";
 
     public void requestMetadata(UUID jobId, String query, String responseQueue) {
         IdeaReportMetadataRequestMessage payload = new IdeaReportMetadataRequestMessage(jobId, query, responseQueue);
