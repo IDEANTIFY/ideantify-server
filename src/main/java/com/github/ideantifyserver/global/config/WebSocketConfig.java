959386e4d439987a4fc380dd2d207437a2116a15
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-11-14 06:00:21 +0900

    feat(ws): 경로 수정

--- src/main/java/com/github/ideantifyserver/global/config/WebSocketConfig.java
+++ src/main/java/com/github/ideantifyserver/global/config/WebSocketConfig.java
@@ -13,10 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
-                .setAllowedOrigins(
-                        "http://localhost:3000",
-                        "https://ideantify.wink.io.kr"
-                );
+                .setAllowedOriginPatterns("*");
     }
 
     @Override
